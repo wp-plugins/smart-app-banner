0189487bf32f1b741f70ba7277ea94e8cbe1b2e5
stephend <stephend@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-07-16 20:40:16 +0000

    Create a settings screen to set the banner to display on the home page
    git-svn-id: https://plugins.svn.wordpress.org/smart-app-banner/trunk@573250 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wsl-smart-app-banner.php
+++ wsl-smart-app-banner.php
@@ -31,17 +31,23 @@ License: GPL
 
 // This just echoes the chosen line, we'll position it later
 function wsl_output_safari_app_banner($post_ID) {
-  // check for properties that give us the app id
-  $custom_fields = get_post_custom($post_ID);
-  $app_id_list = $custom_fields['wsl-app-id'];
-
-  if (is_null($app_id_list)) {
-    // no custom fields; move on
-    return;
+  if (is_front_page()) {
+    $app_id = get_option('wsl_homepage_appid');
   }
+  else {
+    // check for properties that give us the app id
+    $custom_fields = get_post_custom($post_ID);
+    $app_id_list = $custom_fields['wsl-app-id'];
 
-  foreach ( $app_id_list as $key => $value )
-    $app_id = $value;
+    if (is_null($app_id_list)) {
+      // no custom fields; move on
+      return;
+    }
+
+    foreach ( $app_id_list as $key => $value ) {
+      $app_id = $value;
+    }
+  }
 
   // if it's not there, exit
   if (is_null($app_id) or $app_id == "") {
@@ -52,7 +58,80 @@ function wsl_output_safari_app_banner($post_ID) {
   echo "<meta name=\"apple-itunes-app\" content=\"app-id=$app_id\">";
 }
 
-// 
 add_action( 'wp_head', 'wsl_output_safari_app_banner' );
+
+// Admin menu gubbins
+function wsl_smart_app_banner_admin_menu() {
+  add_options_page( 'Smart App Banner Options',
+                    'Smart App Banner',
+                    'manage_options',
+                    'wsl-smart-app-banner',
+                    'wsl_smart_app_banner_options' );
+}
+
+function wsl_smart_app_banner_options() {
+    //must check that the user has the required capability 
+    if (!current_user_can('manage_options'))
+    {
+      wp_die( __('You do not have sufficient permissions to access this page.') );
+    }
+
+    // variables for the field and option names 
+    $opt_name = 'wsl_homepage_appid';
+    $hidden_field_name = 'wsl_submit_hidden';
+    $data_field_name = 'wsl_homepage_appid';
+
+    // Read in existing option value from database
+    $opt_val = get_option( $opt_name );
+
+    // See if the user has posted us some information
+    // If they did, this hidden field will be set to 'Y'
+    if( isset($_POST[ $hidden_field_name ]) && $_POST[ $hidden_field_name ] == 'Y' ) {
+        // Read their posted value
+        $opt_val = $_POST[ $data_field_name ];
+
+        // Save the posted value in the database
+        update_option( $opt_name, $opt_val );
+
+        // Put an settings updated message on the screen
+
+?>
+<div class="updated"><p><strong><?php _e('settings saved.', 'wsl-smart-app-banner' ); ?></strong></p></div>
+<?php
+
+    }
+
+    // Now display the settings editing screen
+
+    echo '<div class="wrap">';
+
+    // header
+
+    echo "<h2>" . __( 'Smart App Banner Settings', 'wsl-smart-app-banner' ) . "</h2>";
+
+    // settings form
+    
+    ?>
+
+<form name="form1" method="post" action="">
+<input type="hidden" name="<?php echo $hidden_field_name; ?>" value="Y">
+
+<p><?php _e("Application ID shown on home page:", 'wsl-smart-app-banner' ); ?> 
+<input type="text" name="<?php echo $data_field_name; ?>" value="<?php echo $opt_val; ?>" size="20">
+</p>
+<p>(Leave blank if no banner is required on the home page.)</p>
+<hr />
+
+<p class="submit">
+<input type="submit" name="Submit" class="button-primary" value="<?php esc_attr_e('Save Changes') ?>" />
+</p>
+
+</form>
+</div>
+
+<?php
+}
+
+add_action( 'admin_menu', 'wsl_smart_app_banner_admin_menu' );
 
 ?>
